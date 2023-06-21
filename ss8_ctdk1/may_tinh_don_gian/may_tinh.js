63febe85a925f84deafc6c6c481f00edc9372557
DienTon <132761242+DienTon@users.noreply.github.com>
2023-06-21 19:21:34 +0700

    first commit

--- ss8_ctdk1/may_tinh_don_gian/may_tinh.js
+++ ss8_ctdk1/may_tinh_don_gian/may_tinh.js
@@ -1,3 +1,11 @@
-function number() {
-    button = document.querySelector("")
+function number(cha) {
+    document.getElementById("input").value+=cha;
+}
+function ketqua() {
+    let kq = eval(document.getElementById("input").value);
+    document.getElementById("input").value=kq;
+
+}
+function reset() {
+    document.getElementById("input").value=" ";
 }
